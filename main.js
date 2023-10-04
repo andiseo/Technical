b74785a99ebaaf91ca5fc991d9e1f17dff26fc93
andiseo <andiseyo50@gmail.com>
2023-10-04 11:37:50 +0700

    JS

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-let barang = ["hp", "charger", "earphone"];
-barang.shift();
-barang.push("case");
-delete barang[1];
-console.log(barang);
+let dmorning = "Andi";
 
-let a = 15;
-let b = 10;
-
-let tambah = a + b;
-let kurang = a - b;
-let kali = a * b;
-let bagi = a / b;
-
-let lebihbesar = a > b;
-let lebihkecil = a < b;
-let samadengan = a === b;
-
-console.log(tambah);
-console.log(kurang);
-console.log(kali);
-console.log(bagi);
-console.log(lebihbesar);
-console.log(lebihkecil);
-console.log(samadengan);
-
-let pengguna = {
-  nama: "Andi",
-  usia: 19,
-  alamat: {
-    jalan: "Silas Papare",
-    Kota: "Biak",
-  },
-  hoby: ["jalan jalan", "ngabisin duit"],
-};
-console.log(pengguna);
+if (dmorning === "Andi") {
+  console.log("Andi Sesi Pagi");
+} else {
+  console.log("Andi Sesi Siang");
+}
