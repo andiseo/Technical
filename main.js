3ecb0401b628f25d7cb2eaa3a82ac8319d40f351
andiseo <andiseyo50@gmail.com>
2023-10-04 11:43:21 +0700

    Switch Case

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-let dmorning = "Andi";
-
-if (dmorning === "Andi") {
-  console.log("Andi Sesi Pagi");
-} else {
-  console.log("Andi Sesi Siang");
+let hari = "Jumat";
+switch (hari) {
+  case "Senin":
+    alert("Hari ini hari senin");
+    break;
+  case "Selasa":
+    alert("Hari ini hari selasa");
+    break;
+  case "Rabu":
+    alert("Hari ini hari rabu");
+    break;
+  case "Kamis":
+    alert("Hari ini hari kamis");
+    break;
+  case "Jumat":
+    alert("Hari ini hari jumat");
+    break;
+  default:
+    alert("Hari ini Weekend");
+    break;
 }
