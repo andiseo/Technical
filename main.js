a2835c854fc91368f8dbaad287731699d4f45134
andiseo <andiseyo50@gmail.com>
2023-10-10 09:44:45 +0700

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let hari = "Jumat";
-switch (hari) {
-  case "Senin":
-    alert("Hari ini hari senin");
-    break;
-  case "Selasa":
-    alert("Hari ini hari selasa");
-    break;
-  case "Rabu":
-    alert("Hari ini hari rabu");
-    break;
-  case "Kamis":
-    alert("Hari ini hari kamis");
-    break;
-  case "Jumat":
-    alert("Hari ini hari jumat");
-    break;
-  default:
-    alert("Hari ini Weekend");
-    break;
+// Classes and Instance
+class Mahasiswa {
+  constructor(nama, nim) {
+    this.nama = nama;
+    this.nim = nim;
+  }
+
+  print() {
+    console.log(`Nama Mahasiswa : ${this.nama}`);
+    console.log(`Nim Mahasiswa : ${this.nim}`);
+  }
 }
+
+let Mahasiswa1 = new Mahasiswa("Andi", 2021061007);
+let Mahasiswa2 = new Mahasiswa("Dhevan", 2021061003);
+
+// inheritance
+
+// encapsulation
+
+// Getter and Setter
